e0c5ea985be4b8b644afe5db4aa954c6b3e022f9
ayushi2410 <ayushi241098@gmail.com>
2020-04-02 08:04:19 +0530

    Updates

---
+++ precache-manifest.71a7f02e400df343dd5b01a5e485a44b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2824d31cca7c24cc84bafb27f2bb9da7",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "bd3e1829d7d5c4ff78ee",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5d31e8d57883a4ea0128",
+    "url": "/robofriends/static/css/main.11fccb34.chunk.css"
+  },
+  {
+    "revision": "bd3e1829d7d5c4ff78ee",
+    "url": "/robofriends/static/js/2.c1a3fb7c.chunk.js"
+  },
+  {
+    "revision": "5d31e8d57883a4ea0128",
+    "url": "/robofriends/static/js/main.444360d0.chunk.js"
+  },
+  {
+    "revision": "1709d8b2ba992f65501f",
+    "url": "/robofriends/static/js/runtime~main.318ca2b2.js"
+  }
+]);
